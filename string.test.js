e404e90bf5e6efc17dfe3187e09064d3f9b5bc3d
usbbush <jeanbienvenusb@gmail.com>
2022-07-12 16:13:28 +0200

    Add new function and test functionalities

--- string.test.js
+++ string.test.js
@@ -1,4 +1,5 @@
-const stringLength = require('./string');
+const stringLength = require('./string-length');
+const reverseString = require('./reverse-string');
 
 test('should return 4 when you pass the string "abed"', () => {
   const testingValue = 'abed';
@@ -6,4 +7,8 @@ test('should return 4 when you pass the string "abed"', () => {
   expect(stringLength(testingValue)).not.toBeLessThan(1);
   expect(stringLength(testingValue)).not.toBeGreaterThan(10);
   expect(stringLength(testingValue)).toBe(4);
+});
+
+test('Should be true when you pass "abc"', () => {
+  expect(reverseString('abc')).toMatch(/cba/);
 });
