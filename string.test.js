d9f4a94e73376781f5085919998dc53c1acd6cfc
usbbush <jeanbienvenusb@gmail.com>
2022-07-12 16:47:32 +0200

    Implement Calculator
     -  write several tests for each tested function
     - Write a simple calculator class or object, which will have 4 methods: add, subtract, divide, and multiply.
     - Write at least 3 tests for each of the calculator methods.
     - Group tests for each method using describe() method.

--- string.test.js
+++ string.test.js
@@ -1,14 +1,91 @@
 const stringLength = require('./string-length');
 const reverseString = require('./reverse-string');
+const Calculator=require('./calculator');
 
-test('should return 4 when you pass the string "abed"', () => {
-  const testingValue = 'abed';
+describe('My String', () => {
+  test('should return 4 when you pass the string "abed"', () => {
+    const testingValue = 'abed';
 
-  expect(stringLength(testingValue)).not.toBeLessThan(1);
-  expect(stringLength(testingValue)).not.toBeGreaterThan(10);
-  expect(stringLength(testingValue)).toBe(4);
+    expect(stringLength(testingValue)).not.toBeLessThan(1);
+    expect(stringLength(testingValue)).not.toBeGreaterThan(10);
+    expect(stringLength(testingValue)).toBe(4);
+  });
+
+  test('Should be true when you pass "abc"', () => {
+    expect(reverseString('abc')).toMatch(/cba/);
+  });
 });
 
-test('Should be true when you pass "abc"', () => {
-  expect(reverseString('abc')).toMatch(/cba/);
+describe('My Calculator', () => {
+  const calculator=new  Calculator();
+
+
+  describe.each([
+    [1, 1, 2],
+    [1, 2, 3],
+    [2, 1, 3],
+  ])('.add(%i, %i)', (a, b, expected) => {
+    test(`returns ${expected}`, () => {
+      expect(calculator.add(a,b)).toBe(expected);
+    });
+
+    test(`returned value not be greater than ${expected}`, () => {
+      expect(calculator.add(a,b)).not.toBeGreaterThan(expected);
+    });
+
+    test(`returned value not be less than ${expected}`, () => {
+      expect(calculator.add(a,b)).not.toBeLessThan(expected);
+    });
+  });
+  describe.each([
+    [1, 1, 0],
+    [1, 2, -1],
+    [2, 1, 1],
+  ])('.subtract(%i, %i)', (a, b, expected) => {
+    test(`returns ${expected}`, () => {
+      expect(calculator.subtract(a,b)).toBe(expected);
+    });
+
+    test(`returned value not be greater than ${expected}`, () => {
+      expect(calculator.subtract(a,b)).not.toBeGreaterThan(expected);
+    });
+
+    test(`returned value not be less than ${expected}`, () => {
+      expect(calculator.subtract(a,b)).not.toBeLessThan(expected);
+    });
+  });
+  describe.each([
+    [1, 1, 1],
+    [1, 2, 2],
+    [2, 3, 6],
+  ])('.multiply(%i, %i)', (a, b, expected) => {
+    test(`returns ${expected}`, () => {
+      expect(calculator.multiply(a,b)).toBe(expected);
+    });
+
+    test(`returned value not be greater than ${expected}`, () => {
+      expect(calculator.multiply(a,b)).not.toBeGreaterThan(expected);
+    });
+
+    test(`returned value not be less than ${expected}`, () => {
+      expect(calculator.multiply(a,b)).not.toBeLessThan(expected);
+    });
+  });
+  describe.each([
+    [1, 1, 1],
+    [1, 2, 0.5],
+    [2, 1, 2],
+  ])('.divide(%i, %i)', (a, b, expected) => {
+    test(`returns ${expected}`, () => {
+      expect(calculator.divide(a,b)).toBe(expected);
+    });
+
+    test(`returned value not be greater than ${expected}`, () => {
+      expect(calculator.divide(a,b)).not.toBeGreaterThan(expected);
+    });
+
+    test(`returned value not be less than ${expected}`, () => {
+      expect(calculator.divide(a,b)).not.toBeLessThan(expected);
+    });
+  });
 });
